5496fb1c00b95bc2038a01a91f33da2259fe5483
Claudiu Stoica <claudiu@maxilia.nl>
2024-12-02 15:47:59 +0200

    initiate plugin

---
+++ src/Storefront/Controller/FastOrderController.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace ShopwareFastOrder\Storefront\Controller;
+
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(defaults: ['_routeScope' => ['storefront']])]
+class FastOrderController extends StorefrontController
+{
+    #[Route(
+        path: '/fast-order',
+        name: 'frontend.fast.order',
+        methods: ['GET']
+    )]
+    public function showForm(Request $request, SalesChannelContext $context): Response
+    {
+        return $this->renderStorefront('@SwFastOrder/storefront/page/form.html.twig', [
+            'example' => 'Hello world'
+        ]);
+    }
+}
